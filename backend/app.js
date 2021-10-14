2f7e753c051991f50e1d7e6fcb0a157d6a2890a0
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-10-14 21:06:28 +0200

    connected to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://new-user-1:n8jdkdmabrEJGVN4@cluster0.jmsjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 const app = express();
