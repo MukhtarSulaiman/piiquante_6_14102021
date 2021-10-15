2c57528d513a0ee9d45c37a67998136850970538
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-10-15 15:32:55 +0200

    Some missing dots have been added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,7 +10,7 @@ exports.signup = (req, res, next) => {
             const user = new User({
                 email: req.body.email,
                 password: hash
-            })
+            });
             user.save()
                 .then(()=> res.status(201).json({ message: 'Utilisateur créé !'}))
                 .catch(error=> res.status(400).json({ error }));
@@ -19,21 +19,21 @@ exports.signup = (req, res, next) => {
 };
 
 
-exports.signin = (req, res, next) => {
+exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
-            then( valid => {
+            .then( valid => {
                 if (!valid) {
                     return res.status(401).json({ error: 'Mot de passe incorrect !' });
                 }
                 res.status(200).json({
                     userId: user._id,
                     token: jwt.sign(
-                        { userId: user_id },
+                        { userId: user._id },
                         'RANDOM_TOKEN_SECRET',
                         { expiresIn: '24h' }
                     )
@@ -43,4 +43,6 @@ exports.signin = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error })); 
 };
+
+
 
