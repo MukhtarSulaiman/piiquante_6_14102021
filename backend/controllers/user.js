acb90d7ffc5a9b3ba7e50a2445f6817bf47755b6
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-10-16 01:22:46 +0200

    A line has been indented

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,7 +26,7 @@ exports.login = (req, res, next) => {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
-            .then( valid => {
+                .then( valid => {
                 if (!valid) {
                     return res.status(401).json({ error: 'Mot de passe incorrect !' });
                 }
