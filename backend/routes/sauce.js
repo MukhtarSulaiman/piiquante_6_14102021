0c65740440c2a1ef41a0e797b83e38ee37500730
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-10-16 01:18:26 +0200

    Added a sauce file to the routes folder

---
+++ backend/routes/sauce.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const sauceCtrl = require('../controllers/sauce');
+const multer = require('../middleware/multer-config')
+
+router.post('/', multer, sauceCtrl.createSauce);
+
+
+module.exports = router;
+
