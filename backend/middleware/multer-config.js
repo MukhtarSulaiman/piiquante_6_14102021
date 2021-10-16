7c7754ffb8ce2a58821f6cca809bb9674a8044b2
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-10-16 18:54:21 +0200

    refactored the extension name

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -4,7 +4,7 @@ const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
-} 
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
@@ -12,9 +12,9 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
-        const extention = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extention);
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-module.exports = multer({ storage }).single('image');
+module.exports = multer({ storage }).single('image')
