94e98b49cbd42d8a6072c8c35f2654dc17f48c63
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-09 12:46:15 +0100

    Replaced the db connection data with envirement variables;

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express'); // Importing the express framework
 const bodyParser = require('body-parser'); // This package allows us to read the body of the request
 const mongoose = require('mongoose'); // It facilates the interactions with the database
+const dotenv = require('dotenv'); // A package that allow us to seprate our sensitive information
+dotenv.config({ path: './config/.env' });
 const path = require('path'); // This module provides utilities for files and directories paths
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
 // Using the connect function to connect to our mongoDB database
-mongoose.connect('mongodb+srv://new-user-1:n8jdkdmabrEJGVN4@cluster0.jmsjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.jmsjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true, 
     useUnifiedTopology: true
 })
